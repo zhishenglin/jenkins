4bb9f241a56e5846a564abb42b242d1dfc36f958
zslin <zslin@onlyou.com>
2020-06-22 21:17:44 +0800

    测试构建

--- src/main/java/com/example/jenkins/controller/MainController.java
+++ src/main/java/com/example/jenkins/controller/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- *测试构建1
+ *测试构建12
  * @Author Created by zslin on 2020/6/18.
  */
 @Controller
